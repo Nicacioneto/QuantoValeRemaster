09b0210d4131406182cb4a845297cd4a9bb9d273
Gabriel Viana <gabrielv14@gmail.com>
2016-03-17 20:55:52 -0300

    Dados do facebook sendo mostrados

--- www/js/Login/LoginCtrl.js
+++ www/js/Login/LoginCtrl.js
@@ -1,14 +1,22 @@
 angular.module('starter')
 
-.controller('LoginCtrl', function($scope) {
+.controller('LoginCtrl', function($scope, $state) {
   $scope.login = function() {
     var ref = new Firebase("https://firefacetest.firebaseio.com");
-    
+
     ref.authWithOAuthPopup("facebook", function(error, authData) {
       if (error) {
+        $scope.logged = function() {
+                        return false;
+                    }
         console.log("Login Failed!", error);
       } else {
-        console.log("Authenticated successfully:", authData);
+        $scope.logged = function() {
+                        return true;
+                    }
+        $scope.user = authData;
+        console.log("Authenticated successfully:", $scope.user);
+        $state.go('app.login');
       }
     });
   }
