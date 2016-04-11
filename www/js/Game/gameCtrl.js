da93cede634504c4f30ee21d6e58d40f051679d4
Sabryna <sabryna.sousa1323@gmail.com>
2016-04-11 11:16:17 -0300

    Limit of jumps of questions

--- www/js/Game/gameCtrl.js
+++ www/js/Game/gameCtrl.js
@@ -2,12 +2,20 @@ angular.module('starter')
 
 .controller('gameCtrl', function($scope, serviceGame, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion) {
 
+  var jump = 0;
 
-  $scope.bu = function() {
-    alert("jjj");
+  $scope.jump = function() {
     $scope.values = serviceGame.buttonPress().get();
   }
 
+  $scope.jump_question = function() {
+    $scope.values = serviceGame.buttonPress().get();
+    jump += 1;
+    console.log("JUMP == ", +jump)
+    if ( jump > 3 ){
+      alert("Você atingiu o limite máximo!");
+    }
+  }
 
   $scope.$parent.showHeader();
   $scope.$parent.clearFabs();
